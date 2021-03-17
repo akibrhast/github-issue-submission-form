7002a98b87c2ce479f44713ab35b1051a30620ab
Akib Mohaimenur Rahman <akib.rhast@gmail.com>
2021-03-17 18:36:45 -0400

    moved to const

---
+++ assets/js/const.js
@@ -0,0 +1,7 @@
+const BASE_CORS_URI = 'https://secure-brook-93015.herokuapp.com/';
+const CLIENT_ID = '0f4d20ae24b10b1b1e45';
+const CLIENT_SECRET = '698886ef07bc854459044d012b5be122472795f6';
+const REDIRECT_URI = `https://akibrhast.github.io/github-issue-submission-form/callback`;
+const OWNER = 'hackforla';
+const REPO = 'website';
+const CREATE_ISSUE = `${BASE_CORS_URI}https://api.github.com/repos/${OWNER}/${REPO}/issues`;
